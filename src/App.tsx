b700436ff86330f126c7ffbeff775814e12f84bf
Wagner <wmaluk@gmail.com>
2022-07-22 19:00:42 -0300

    add actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './paginas/Home';
-import VitrineRestaurantes from './paginas/VitrineRestaurantes';
+import { Routes, Route } from "react-router-dom";
+import AdministracaoRestaurantes from "./paginas/Administracao/Restaurantes";
+import FormularioRestaurante from "./paginas/Administracao/Restaurantes/Formulario";
+import Home from "./paginas/Home";
+import VitrineRestaurantes from "./paginas/VitrineRestaurantes";
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+      <Route
+        path="/admin/restaurantes"
+        element={<AdministracaoRestaurantes />}
+      />
+      <Route
+        path="/admin/restaurantes/novo"
+        element={<FormularioRestaurante />}
+      />
+      <Route
+        path="/admin/restaurantes/:id"
+        element={<FormularioRestaurante />}
+      />
     </Routes>
   );
 }
